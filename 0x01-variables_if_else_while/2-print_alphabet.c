04d2833960aa4ac98ae740fedcd68637f3a97b5e
Wandile <Wandilenkovu22@gmail.com>
2023-07-15 13:27:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main -Entry point
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+putchar(65);
+putchar('\n');
+return (0);
+}	
