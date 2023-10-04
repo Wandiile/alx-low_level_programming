1131ee279b431959b2c4477084389fae102f9445
Wandile <wandilenkovu22@gmail.com>
2023-10-04 15:45:42 -0700

    whats my name

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _putchar - writes the character tp stdout
+* @c: character to be printed
+* Return: 1 on success, -1 on error
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, -1));
+}
